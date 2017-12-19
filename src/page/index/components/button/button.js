cf63eb99decb44f2435dc1d27820fc115b46a460
Foreverofh <598220654@qq.com>
2017-12-19 17:33:25 +0800

    所以的权限都是后台接口提供

--- src/page/index/components/button/button.js
+++ src/page/index/components/button/button.js
@@ -2,11 +2,12 @@
 import buttonTemplate from "./button.art"
 
 
-// 请求回来权限列表
+// ajax请求回来权限列表， 不能存在sessionStorage
 var roles = [1, 2, 3];
 
-// 用户登录获取他的权限编码
+// 用户登录获取他的权限编码  不能存在sessionStorage,容易修改，权限无作用
 var userRoles = 1;
+
 
 // role 参数是根据权限来渲染对应html
 const buttonHtml = buttonTemplate({button: "哈哈", header: "头部", role: roles.indexOf(userRoles)});
